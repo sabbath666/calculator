843e6cb80abdbf429b72b8bc45a94253af1a32b8
sabbath <sergeysabbath@gmail.com>
2018-09-14 17:46:04 +0300

    add ansible

--- vars/makeScript.groovy
+++ vars/makeScript.groovy
@@ -3,3 +3,14 @@ def src = new File("/home/jenkins/workspace/calculator/script.src")
 def src2 = new File("/home/jenkins/workspace/calculator/script-update.src")
 src2 << src.text
 src2 << f.text
+ZipOutputStream zipFile = new ZipOutputStream(new FileOutputStream("update.zip"))
+src.text.eachLine {
+    zipFile.putNextEntry(new ZipEntry(it))
+    def buffer = new byte[file.size()]
+    file.withInputStream {
+        zipFile.write(buffer, 0, it.read(buffer))
+    }
+    zipFile.closeEntry()
+}
+zipFile.close()
+
