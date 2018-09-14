e8d9c873fc97c2225f377a4b2e398ab41ba4d37c
sabbath <sergeysabbath@gmail.com>
2018-09-14 16:13:21 +0300

    add ansible

--- vars/makeScript.groovy
+++ vars/makeScript.groovy
@@ -1,5 +1,5 @@
 def call(){
-    prinln("current dir ${new File(".").canonicalPath}")
+    println("current dir ${new File(".").canonicalPath}")
     def f = new File("test.txt")
     def src = new File("script.src")
     src << f.text
