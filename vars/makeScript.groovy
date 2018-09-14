6195bba2a9057fce171956f9af3ddabb9328d9a3
sabbath <sergeysabbath@gmail.com>
2018-09-14 17:27:56 +0300

    add ansible

--- vars/makeScript.groovy
+++ vars/makeScript.groovy
@@ -1,7 +1,9 @@
-def call(def filepath){
-
+def call(String filepath){
     def f = new File(filepath)
-    def src = new File("script.src")
+    def src = new File("./script.src")
+    src << "---------------------------"
     src << f.text
     println ("script.src: ${src.text}")
+    def src2 = new File("./script-2.src")
+    src2.write("dfgdfgdfgdfdfgdfngmdfng")
 }
